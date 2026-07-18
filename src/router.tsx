eb51bcead6de4f88d7e8efc9562c0108624c42bd
Liliana Mendoza <liliana.sarahi@gmail.com>
2026-07-18 21:18:14 +0000

    update router

--- src/router.tsx
+++ src/router.tsx
@@ -5,12 +5,17 @@ import { routeTree } from "./routeTree.gen";
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
+  // Removemos la barra diagonal del final si existe (ej. "/repo/" pasa a "/repo")
+  // Si es solo "/", lo dejamos como una cadena vacía "" para que TanStack lo entienda bien
+  const base = import.meta.env.BASE_URL;
+  const cleanedBasepath = base === '/' ? '/' : base.replace(/\/$/, '');
+
   const router = createRouter({
     routeTree,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    basepath: import.meta.env.BASE_URL || "/",
+    basepath: cleanedBasepath, // ◄--- Usamos la ruta limpia sin el '/' al final
   });
 
   return router;
